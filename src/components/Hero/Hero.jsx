f917acab9c2c73772716baf72339a13806721a7e
VedantSG123 <vedantghonge0@gmail.com>
2023-04-28 23:57:43 +0530

    switched to neubrutalist ui

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,13 +5,18 @@ import Image from 'next/image'
 export default function Hero() {
   return <>
     <div className={`${styles.heroWrapper}`}>
-      <Image src='/hero/hero-main.jpg' layout='fill' />
+      <Image src='/hero/hero-main.png' layout='fill' />
       <div className={`${styles.hero}`}>
-        <div className={styles.heroMain}>
-          <h1>Summer of Innovation 2023</h1>
-          <div className={styles.heroQuoteWrapper}>
-            <div className={styles.styleBar}></div>
-            <div className={styles.Quote}>Write your quote here</div>
+        <div className={styles.heroContentWrapper}>
+          <div className={styles.heroMain}>
+            <h1>Summer of Innovation 2023</h1>
+            <div className={styles.heroQuoteWrapper}>
+              <div className={styles.styleBar}></div>
+              <div className={styles.Quote}>Write your quote here</div>
+            </div>
+          </div>
+          <div className={styles.heroArtWrapper}>
+            <Image src='/hero/hero-art.svg' layout='fill' />
           </div>
         </div>
       </div>
