ec4d9f6810b9722f9096543500f2775d4b8bb529
Vidyasagar Singadi <vidyasagar.singadi@gmail.com>
2023-05-12 16:44:46 +0530

    refined team stylesheet

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -7,9 +7,10 @@ export default function Teams() {
     <div className={styles.teamBody}>
       <div className={`${styles.teamWrapper}`}>
         <div className={styles.teamsContentWrapper}>
-          <div className={styles.teamHeaderWrapper}><div className={styles.teamHeader}>
-            <h1>Meet the Team</h1>
-          </div>
+          <div className={styles.teamHeaderWrapper}>
+            <div className={styles.teamHeader}>
+              <h1>Meet the Team</h1>
+            </div>
           </div>
           {teamsData.map((team) => (
             <div key={team.name} className={styles.teamContainer}>
@@ -30,21 +31,21 @@ export default function Teams() {
                       <div className={styles.memberInfo}>
                         <h3>{member.name}</h3>
                         <div className={styles.memberLinks}>
-                          <a href={`mailto:${member.email}`} className={styles.linkImageWrapper}>
+                          <a href={`mailto:${member.email}`} className={styles.linkImageWrapper + " " + styles.email}>
                             <Image className={styles.linkImage} src='/Team/Logo/gmail1.png' width={20} height={20}/>
                           </a>
                           {member.linkedin && (
-                          <a className={styles.linkImageWrapper} href={member.linkedin} target="_blank" rel="noopener noreferrer">
+                          <a className={styles.linkImageWrapper + " " + styles.linkedin} href={member.linkedin} target="_blank" rel="noopener noreferrer">
                             <Image className={styles.linkImage} src='/Team/Logo/linkedin1.png' width={20} height={20} />
                           </a>
                           )}
                           {member.twitter && (
-                          <a className={styles.linkImageWrapper} href={member.twitter} target="_blank" rel="noopener noreferrer">
+                          <a className={styles.linkImageWrapper + " " + styles.twitter} href={member.twitter} target="_blank" rel="noopener noreferrer">
                             <Image className={styles.linkImage} src='/Team/Logo/twitter1.png' width={20} height={20} />
                           </a>
                           )}
                           {member.instagram && (
-                          <a className={styles.linkImageWrapper} href={member.instagram} target="_blank" rel="noopener noreferrer">
+                          <a className={styles.linkImageWrapper + " " + styles.instagram} href={member.instagram} target="_blank" rel="noopener noreferrer">
                             <Image className={styles.linkImage} src='/Team/Logo/instagram.png' width={20} height={20} />
                           </a>
                           )}
