31a982c62fedddcab26ea89a3447cdb0532ac6d7
VedantSG123 <vedantghonge0@gmail.com>
2023-05-19 15:23:16 +0530

    favicon for teams page

--- src/components/Events/EventCard/EventCard.jsx
+++ src/components/Events/EventCard/EventCard.jsx
@@ -1,36 +1,44 @@
-import styles from './EventCard.module.css'
-import Image from 'next/image'
+import styles from "./EventCard.module.css";
+import Image from "next/image";
 
-export default function EventCard({leftSideImage, data, number}){
-  return <>
-    <div className={styles.eventCardWrapper}>
-      <div className={styles.eventCardMain}>
-        <div className={`${styles.eventInfo} ${leftSideImage && styles.eventInfoLeft}`}>
-          <div className={styles.eventNumber}>
-            <h1>{number<10 ? `0${number}` : number}</h1>
-            <div className={styles.styleDiv}></div>
+export default function EventCard({ leftSideImage, data, number }) {
+  return (
+    <>
+      <div className={styles.eventCardWrapper}>
+        <div className={styles.eventCardMain}>
+          <div
+            className={`${styles.eventInfo} ${
+              leftSideImage && styles.eventInfoLeft
+            }`}
+          >
+            <div className={styles.eventNumber}>
+              <h1>{number < 10 ? `0${number}` : number}</h1>
+              <div className={styles.styleDiv}></div>
+            </div>
+            <div className={styles.eventName}>
+              <h1>{data.heading}</h1>
+            </div>
+            <div className={styles.eventInfo}>{data.content}</div>
+            <div className={styles.knowMore}>
+              <a href={data.knowMoreLink}>
+                <div>Know More</div>
+                <div className={styles.buttonImage}>
+                  <Image src="/Events/event-button.svg" fill priority />
+                </div>
+              </a>
+            </div>
           </div>
-          <div className={styles.eventName}>
-            <h1>{data.heading}</h1>
-          </div>
-          <div className={styles.eventInfo}>
-            {data.content}
-          </div>
-          <div className={styles.knowMore}>
-            <a href={data.knowMoreLink}>
-              <div>Know More</div>
-              <div className={styles.buttonImage}>
-                <Image src='/Events/event-button.svg' fill />
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className={`${styles.eventImageWrapper} ${leftSideImage && styles.eventImageWrapperLeft}`}>
-          <div className={styles.eventImageCard}>
-            <Image src={data.image} fill />
+          <div
+            className={`${styles.eventImageWrapper} ${
+              leftSideImage && styles.eventImageWrapperLeft
+            }`}
+          >
+            <div className={styles.eventImageCard}>
+              <Image src={data.image} fill />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
