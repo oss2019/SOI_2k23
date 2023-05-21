e410a665fcc5339dc52c9a0451077ab7a35cc0b9
VedantSG123 <vedantghonge0@gmail.com>
2023-05-21 16:53:16 +0530

    fixed event page sun

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,40 +1,40 @@
-import Image from "next/image";
-import GSAP from 'gsap'
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-import { useEffect, useRef } from 'react'
-import styles from "./Events.module.css";
-import EventCard from "./EventCard/EventCard";
-import EventsData from "./data";
+import Image from "next/image"
+import GSAP from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+import { useEffect, useRef } from "react"
+import styles from "./Events.module.css"
+import EventCard from "./EventCard/EventCard"
+import EventsData from "./data"
+
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || ""
 
 export default function Events() {
   const SunRef = useRef()
-  
+
   GSAP.registerPlugin(ScrollTrigger)
   useEffect(() => {
     const Sun = SunRef.current
     const timeLine = GSAP.timeline({
-      scrollTrigger:{
-        trigger:Sun,
-        start:'top bottom',
-        end:'bottom top',
-        scrub:1
-      }
+      scrollTrigger: {
+        trigger: Sun,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1,
+      },
     })
 
     timeLine.to(Sun, {
-      rotation:30
+      rotation: 30,
     })
-  },[])
-
-  
+  }, [])
 
   return (
     <>
       <div id="eventsId#123" className={styles.eventsWrapper}>
-      <div className={styles.eventSunWrapper} ref={SunRef}>
-        <Image src='/Events/events-sun-1.svg' fill priority />
-      </div>
-      <div className={`${styles.eventsMain} page-wrapper`}>
+        <div className={styles.eventSunWrapper} ref={SunRef}>
+          <Image src={prefix + "/Events/events-sun-1.svg"} fill priority />
+        </div>
+        <div className={`${styles.eventsMain} page-wrapper`}>
           <div className={styles.eventsHeading}>
             <div className={styles.text_block}>
               <h2>Events</h2>
@@ -49,11 +49,11 @@ export default function Events() {
                   number={index + 1}
                   leftSideImage={index % 2 ? false : true}
                 />
-              );
+              )
             })}
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
