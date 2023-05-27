f4d3a3312087947b72ed64b37a6dae58ed742bde
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-05-27 20:13:52 +0530

    some changes made

--- src/components/Events/data.js
+++ src/components/Events/data.js
@@ -7,17 +7,6 @@ const data = [
     knowMoreLink:
       "https://drive.google.com/file/d/1HV3reLcxYTSBijPtsfizEHURbx_C__c_/view?usp=sharing",
   },
-  {
-    heading: "Hack The Trade",
-    content: `Price action trading is a strategy favored by day traders that
-             involves making decisions based on recent price movements
-            rather than relying on indicators or analysis. Common tools
-            include technical patterns, support and resistance levels,
-            candlestick patterns, and trend analysis.`,
-    image: "/Events/stockmarketimage.jpg",
-    knowMoreLink:
-      "https://drive.google.com/file/d/1c8qUvDKzilgfZPjwe5jAKmab32ayNPK2/view?usp=sharing",
-  },
 ]
 
 export default data
