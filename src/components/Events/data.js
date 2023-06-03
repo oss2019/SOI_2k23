88354365b41f5df4dfc85ae8a1bc7f5917dc559b
Vidyasagar Singadi <vidyasagar.singadi@gmail.com>
2023-06-03 19:59:59 +0530

    AIwars added

--- src/components/Events/data.js
+++ src/components/Events/data.js
@@ -24,6 +24,13 @@ const data = [
     knowMoreLink:
       "https://drive.google.com/file/d/1UpLDai46hG_Xhu4E1YjxeHKwe82lXAaV/view?usp=sharing",
   },
+  {
+    heading: "AIWars: Conquer the Battlefield",
+    content: `The game of "Dots and Boxes" begins with an empty grid of dots. Players take turns adding a line of unit length between two unconnected dots. When a player completes the fourth side of a 1x1 box, they earn a point and get another turn. Your goal is to code a bot which performs good at the game.`,
+    image: "/Events/AIwars.jpg",
+    knowMoreLink:
+      "https://drive.google.com/file/d/1I-VS6n45cYAmGtECH3lCGQpSUYQzBIU-/view",
+  },
 ]
 
 export default data
