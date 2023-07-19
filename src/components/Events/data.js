ba504868c79ee3dfc24cd9821c3cd30a1baaa38e
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-07-19 17:03:33 +0530

    added bitwise symphony event

--- src/components/Events/data.js
+++ src/components/Events/data.js
@@ -90,6 +90,13 @@ const data = [
     knowMoreLink:
       "https://drive.google.com/file/d/1c8qUvDKzilgfZPjwe5jAKmab32ayNPK2/view?usp=sharing",
   },
-]
+  {
+    heading: "Bitwise Symphony",
+    content: `Unleash your algorithmic prowess, crafting a symphony from integers. Witness the mesmerizing rise of numbers, powered by bitwise sorcery. Seek the harmonies that transcend ordinariness, forging an ascending masterpiece. Dare you compose the ultimate melody, or chase the enigmatic crescendo? Enter the realm of Bitwise Symphony!.`,
+    image: "/Events/sar.png",
+    knowMoreLink:
+      "https://drive.google.com/file/d/1oYef04uoGGo2glUbUzRyPpaeH74GXIer/view?usp=sharing",
+  },
+];
 
-export default data
+export default data;
